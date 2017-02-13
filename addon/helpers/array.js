dbb16816973af83dec0dfa49032fc45dac2bad7a
kellyselden <kellyselden@gmail.com>
2017-02-13 14:04:20 -0800

    update comment (reverted from commit ee883d68b8b16d8a5d338ed88146c8d00038aefd)
    fix prototype extension bug with ember beta (reverted from commit 864697f27c05db2b0b21bfd63d19b48532fe5aef)

--- addon/helpers/array.js
+++ addon/helpers/array.js
@@ -1,17 +1,7 @@
 import Ember from 'ember';
 
-const {
-  Helper: { helper },
-  A: newArray
-} = Ember;
+export function array(params/*, hash*/) {
+  return params;
+}
 
-export default helper(params => {
-  // this has a bug in ember 2.10
-  // https://github.com/emberjs/ember.js/pull/14649
-  // let array = newArray(params);
-
-  let array = newArray();
-  array.pushObjects(params);
-
-  return array;
-});
+export default Ember.Helper.helper(array);
