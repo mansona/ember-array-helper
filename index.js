5dbe7953e6fc1283f6df5d9c94a29c35f894a6cb
Chris Manson <chris@manson.ie>
2020-01-23 11:23:42 +0000

    only enable for ember < 3.8

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const VersionChecker = require('ember-cli-version-checker');
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  treeFor() {
+    let checker = new VersionChecker(this.project);
+    let ember = checker.forEmber();
+
+    if (ember.isAbove('3.8.0')) {
+      return null;
+    }
+
+    return this._super(...arguments);
+  }
 };
